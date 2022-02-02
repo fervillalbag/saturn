d73bf5aa62cd703881d3700d408e1e9b5762b3f3
fervillalbag <21fervillalbag@gmail.com>
2022-02-02 10:18:07 -0300

    feat: page index fetch data and form create implemented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,56 @@
 import React from 'react'
+import { GetServerSideProps } from 'next'
 
-const Home: React.FC = () => {
+import { Post } from '@/types/Post'
+import { useRouter } from 'next/dist/client/router'
+
+interface HomeIprops {
+  posts: Post[]
+}
+
+type DataType = {
+  props: {
+    posts: Post[]
+  }
+}
+
+const Home: React.FC<HomeIprops> = ({ posts }) => {
+  const router = useRouter()
+
   return (
-    <div>
-      <h1>Starting the project</h1>
+    <div className="p-4">
+      {posts.length === 0 ? (
+        <span className="block">there are no posts</span>
+      ) : (
+        posts.map((post: Post) => (
+          <div key={post._id}>
+            <span className="block">{post.title}</span>
+          </div>
+        ))
+      )}
+
+      <div className="mt-2">
+        <button
+          className="border p-2"
+          onClick={() => router.push('post/create')}
+        >
+          create a post
+        </button>
+      </div>
     </div>
   )
 }
+
+export const getServerSideProps: GetServerSideProps =
+  async (): Promise<DataType> => {
+    const res = await fetch('http://localhost:3000/api/post')
+    const posts = await res.json()
+
+    return {
+      props: {
+        posts
+      }
+    }
+  }
 
 export default Home
---
+++ src/pages/post/create.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+interface PostData {
+  title: string
+  description: string
+}
+
+const PostCreate: React.FC = () => {
+  const [postData, setPostData] = useState<PostData>({
+    title: '',
+    description: ''
+  })
+
+  const handleCreatePost = async () => {
+    try {
+      const newPost = await fetch('http://localhost:3000/api/post', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+      })
+      console.log(newPost)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <div className="p-4">
+      <span className="block">post create</span>
+
+      <div className="mt-4">
+        <div className="py-4">
+          <input
+            type="text"
+            className="border p-2 w-full"
+            value={postData.title}
+            onChange={e => setPostData({ ...postData, title: e.target.value })}
+          />
+        </div>
+        <div className="py-4">
+          <textarea
+            className="w-full border p-2 h-32"
+            value={postData.description}
+            onChange={e =>
+              setPostData({ ...postData, description: e.target.value })
+            }
+          ></textarea>
+        </div>
+        <div className="py-4">
+          <button className="border p-2" onClick={handleCreatePost}>
+            submit
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PostCreate
